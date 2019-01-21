90605b2094bc2c0c81fbb90363914c440b566fd6
sophiaken <42984186+sophiaken@users.noreply.github.com>
2019-01-21 10:32:06 -0500

    ACA project

---
+++ ACA_PROJECT/ACA_PROJECT/ML CODE/SVM/Implementation_gpu_CODES/SVM_GPU_WITHOUT_THREADS.py
@@ -0,0 +1,67 @@
+import time
+import timeit
+import tensorflow as tf
+import numpy as np
+from sklearn.ensemble import BaggingClassifier
+from sklearn import datasets
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+
+print("started")
+import time
+import timeit
+import tensorflow as tf
+import numpy as np
+from sklearn.ensemble import BaggingClassifier
+from sklearn import datasets
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.svm import SVC
+
+print("started")
+
+iris = datasets.load_iris()
+X, y = iris.data, iris.target
+
+X = np.repeat(X, 100, axis=0)
+y = np.repeat(y, 100, axis=0)
+
+# See https://www.tensorflow.org/tutorials/using_gpu#allowing_gpu_memory_growth
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+
+
+sess = tf.Session(config=config)
+
+with tf.device('/gpu:0'):
+  start = time.time()
+  clf = OneVsRestClassifier(SVC(kernel='linear', probability=True, class_weight='balanced'))
+  clf.fit(X, y)
+  end = time.time()
+  print ("Single SVC gpu", end - start, clf.score(X,y))
+  proba = clf.predict_proba(X)
+  t=tf.convert_to_tensor(proba, dtype=tf.float32)
+
+  n_estimators = 10
+  start = time.time()
+  clf = OneVsRestClassifier(BaggingClassifier(SVC(kernel='linear', probability=True, class_weight='balanced'), max_samples=1.0 / n_estimators, n_estimators=n_estimators, bootstrap=False))
+  clf.fit(X, y)
+  end = time.time()
+  print ("Bagging SVC gpu", end - start, clf.score(X,y))
+  proba = clf.predict_proba(X)
+  t=tf.convert_to_tensor(proba, dtype=tf.float32)
+  
+
+
+def gpu():
+  sess.run()
+  
+
+sess.close()
+
+
+
+    
+   
+
+
+
---
+++ ACA_PROJECT/ACA_PROJECT/README.txt
@@ -0,0 +1,5 @@
+Instructions:
+1)All programs must be executed with python 3.5 or above.
+2) Google colab is the platform that we used for GPU and CPU. Executing the programs in that environment will be accurate for results.
+3)For GPU, the google colab notebook settings has to be changed from None to GPU in order to run the GPU code in the colab environment.
+4)Tensor flow packages and scikit libraries has to be installed in the Google colab for execution of programs.
